01ea73e7d7b1c32f84dd3622049a5b64f16a409c
corbinarnett <corbin.c.arnett@gmail.com>
2020-01-21 22:42:07 -0800

    add controller actions to reviews controller

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,3 +1,38 @@
 class Api::V1::ReviewsController < ApplicationController
 
+  before_action :set_album
+
+  def index
+    @reviews = @album.reviews 
+    render json: @reviews
+  end
+
+  def create
+    @review = @account.reviews.build(review_params)
+    if @review.save
+      render json: @review
+    else
+      render json:{error: "Error creating review"}, status: 400
+    end
+  end
+
+  def show
+    @review = @album.reviews.find_by(id: params[:id])
+    render json: @review
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def set_album
+    # /api/v1/albums/:id/reviews
+    @album = Album.find(params[:album_id])
+  end
+  
+  def review_params
+    params.require(:review).permit(:content, :username, :rating, :album_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   
   namespace :api do 
     namespace :v1 do
-      resources :reviews
-      resources :albums
+      resources :albums do
+        # only access reviews through albums like so, api/v1/albums/1/reviews
+        resources :reviews
+      end
     end
   end
   
