a7fdded7ddd72956694e7975fb87807862e7a642
corbinarnett <corbin.c.arnett@gmail.com>
2020-01-21 20:43:07 -0800

    add model associations and validations

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,2 +1,4 @@
 class Album < ApplicationRecord
+  has_many :reviews
+  validates :artist, :label, :release_date, :img_url, presence: true
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,4 @@
 class Review < ApplicationRecord
+  belongs_to :album
+  validates :content, :username, :rating, presence: true
 end
