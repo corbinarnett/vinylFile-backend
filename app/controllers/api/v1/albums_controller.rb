d728c7d4fdc0bb7e986518c22e8806eb1cece722
corbinarnett <corbin.c.arnett@gmail.com>
2020-01-21 22:14:44 -0800

    add controller actions to album controller

--- app/controllers/api/v1/albums_controller.rb
+++ app/controllers/api/v1/albums_controller.rb
@@ -1,2 +1,34 @@
 class Api::V1::AlbumsController < ApplicationController
+  
+  def index
+    @albums = Album.all
+    render json: @albums
+  end
+
+  def create
+    @album = Album.new(album_params)
+    if @album.save
+      render json: @album
+    else
+      render json: {error: 'Error creating album'}
+    end
+  end
+
+  def show
+    # /api/v1/albums/:id
+    @album = Album.find(params[:id])
+    render json: @album
+  end
+
+  def destroy
+    @album = Album.find(params[:id])
+    @album.destroy
+    render json: @album
+  end
+
+  private
+  
+  def album_params
+    params.require(:album).permit(:name, :artist, :label, :release_date, :image_url)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
 album = Album.create(name: "Chet Baker Sings", artist: "Chet Baker", label: "Pacific Jazz", release_date: "1954", image_url: "https://img.discogs.com/0q_xj68gTVDNMtPkpwKPZGscfgA=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-2930625-1437056636-5751.jpeg.jpg")
 
-review = Review.new(content: "Lorem Ipsum", username: "corbinarnett", rating: 9.8, album_id: 1)
+review = Review.create(content: "Lorem Ipsum", username: "corbinarnett", rating: 9.8, album_id: 1)
